fafa75c5c3c519c782745564f6ce05b55f180a92
BigBooleanJohn <119363172+BigBooleanJohn@users.noreply.github.com>
2023-11-23 10:17:29 -0600

    Update QuickCalculator.java

--- src/QuickCalculator.java
+++ src/QuickCalculator.java
@@ -18,17 +18,17 @@ public class QuickCalculator {
      * 
      * 
      */
-    public static boolean containsInt(String st) {// if we are at an empty string or a space
+    public static boolean containsInt(String st) {
         if (st.compareTo("") == 0 || st.compareTo(" ") == 0) {
             return false;
-        }
+        }//if
         for (int i = 0; i < st.length(); i++) {
-            if (st.toCharArray()[i] - '0' > 0 || st.toCharArray()[i] - '0' < 9)// if the string contains an integer
+            if (st.toCharArray()[i] - '0' > 0 || st.toCharArray()[i] - '0' < 9)
             {
                 return true;
             }
-        }
-        return false;// if no case was met that would constitute an operator
+        }//for
+        return false;
     }// containsInt
 
     public static void main(String[] args) {
@@ -38,8 +38,8 @@ public class QuickCalculator {
                 "this is a program that will prompt you for input once, and then print it out. please put each individual command in quotes, such as \"5 + 12\" and a space between each command:");
         String input = sc.nextLine();
         sc.close();
-        String[] commands = input.split("\"");// breaking apart the string array into it's individual commands
-        int MAX_VALS = 100;// setting max number of registers we can hold
+        String[] commands = input.split("\"");
+        int MAX_VALS = 100;
         BigFraction result = null;
         BigFraction[] fractions = new BigFraction[MAX_VALS];
         char[] registerArray = new char[MAX_VALS];
@@ -48,13 +48,13 @@ public class QuickCalculator {
         for (int i = 0; i < commands.length; i++) {
             if (commands[i].contains("STORE") == true) {
                 String[] StoreArr = commands[i].split(" ");
-                calc.store(StoreArr[1].toCharArray()[0], result, fractions, registerArray, pen);// passing the input and
+                calc.store(StoreArr[1].toCharArray()[0], result, fractions, registerArray, pen);
             } else if (containsInt(commands[i]) == true) {
-                boolean check = calc2.InitialErrorHandling(commands[i], pen);// handling errors
+                boolean check = calc2.InitialErrorHandling(commands[i], pen);
                 if (check != false) {
-                    result = calc2.Calculator(calc, commands[i], fractions, registerArray, pen);// passing the input to
+                    result = calc2.Calculator(calc, commands[i], fractions, registerArray, pen);
                 }
             }
-        }
+        }//for
     }// main
 }
